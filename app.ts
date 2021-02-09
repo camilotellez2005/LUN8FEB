4e177f0840421939589349cd289009b90805cb79
camilotellez2005 <77992505+camilotellez2005@users.noreply.github.com>
2021-02-08 20:32:11 -0500

    Add files via upload

---
+++ app.ts
@@ -0,0 +1,40 @@
+import express from 'express';
+
+const app = express();
+app.use(express.json());
+
+app.get('/',(req,res)=>{
+    res.send('hola mundo')
+});
+
+let productos: any[]=[];
+
+app.get('/productos',(req,res)=>{
+    res.json(productos)
+});
+
+app.post('/productos',(req,res)=>{
+    const {id, title, price, thumbnail} = req.body
+    const producto = {
+        id:id,
+        title:title,
+        price:price,
+        thumbnail:thumbnail,
+    }
+    productos.push(producto)
+    res.sendStatus(201)
+})
+
+
+app.get('/gatos/:id',(req,res)=>{
+    const id = req.params.id
+    const producto = productos.find(gato=> gato.id === id)
+    if (!producto) {
+        res.sendStatus(404)
+    }
+   res.json(producto)
+})
+
+
+
+app.listen(5000,()=>console.log('server up'));
